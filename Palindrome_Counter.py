58cc3c52f74e8090f075210b8e55504d0896e4e6
sdeepugd <sdeepugd@gmail.com>
2019-04-23 02:04:25 +0530

    Create Palindrome_Counter.py

---
+++ Palindrome_Counter.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+import sys
+
+def ispalindrome(str):
+    n=len(str)
+    for i in range(n/2):
+        if str[i]!=str[n-i-1]:
+            return False
+    return True
+
+if __name__=="__main__":
+    palin_set=set()
+    strng=sys.argv[1]
+    
+    def genpalin(start,stop,n):
+        if start>stop or stop is n or start is n:
+            return
+        else:
+            indices=(start,stop)
+            palin_set.add(indices)
+        genpalin(start+1,stop,n)
+        genpalin(start,stop+1,n)
+    
+    genpalin(0,0,n=len(strng))
+    count=0
+    for indices in palin_set:
+        start=indices[0]
+        stop=indices[1]
+        substrng=strng[start:stop+1]
+        if ispalindrome(substrng):
+            count=count+1
+    print count
